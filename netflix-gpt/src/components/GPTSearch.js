5988025b9f4e63496af075e5d67ce5368b2dde69
SUNKARA Rohith <SKR74@dsone.3ds.com>
2024-04-15 16:50:40 +0530

    .

--- netflix-gpt/src/components/GPTSearch.js
+++ netflix-gpt/src/components/GPTSearch.js
@@ -2,10 +2,13 @@ import { LanguageConstants } from "../utils/LanguageConstants";
 import { useSelector } from "react-redux";
 
 const GPTSearch = () => {
+    const handleOnSubmitOfForm = (e)=>{
+        e.preventDefault();
+    }
     const selectedLanguage = useSelector(store=>store.language.selectedLan);
 
     return <div className="pt-[10%] flex justify-center relative z-4">
-    <form className="w-1/2 bg-black grid grid-cols-12">
+    <form className="w-1/2 bg-black grid grid-cols-12" onSubmit={handleOnSubmitOfForm}>
          <input type="text" placeholder={LanguageConstants[selectedLanguage].inputPlaceholder} className="p-4 m-4 col-span-9"></input>
          <button className="col-span-3 m-4 py-2 px-4 bg-red-700 rounded-md text-white">{LanguageConstants[selectedLanguage].searchLabel}</button>
     </form>
